e47cf4f9948e3a8d28f8e40cd27213c0630631c4
Eli Jones <elijah.jones@siu.edu>
2022-11-17 12:13:49 -0600

    First edits

---
+++ src/ChemicalFormula.java
@@ -0,0 +1,42 @@
+public abstract class ChemicalFormula {
+    private static final String[] ATOMIC_SYMBOLS = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+            "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
+            "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+            "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr",
+            "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
+            "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
+            "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+            "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg",
+            "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
+            "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
+            "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
+            "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"};
+    protected Integer[] elementsArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0};
+    public ChemicalFormula(String chemicalFormulaString) {
+        this.elementsArray = toArray(chemicalFormulaString, elementsArray);
+    }
+    public ChemicalFormula() {
+        this("");
+    }
+    public String toString() {
+        String tempString = "";
+        for(int i = 0; i < 118; i++) {
+            if(elementsArray[i] != 0) {
+                tempString += ATOMIC_SYMBOLS[i] + elementsArray[i];
+            }
+        }
+        return tempString;
+    }
+    protected abstract Integer[] toArray(String chemicalFormulaString, Integer[] elementArray);
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ProteinChemicalFormula myForm = new ProteinChemicalFormula("Val-Gly-Ala");
+        System.out.println(myForm.toString());
+    }
+}
